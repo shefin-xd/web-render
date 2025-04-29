003f5e14bb1aab2d72aa243b340e8594c48b74a5
shefin. <shefinfranklin03@gmail.com>
2025-04-29 19:27:45 +0530

    Update auth.route.js

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { checkAuth, login, logout, signup, updateProfile, deleteProfile, getLastSeen } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -11,5 +11,8 @@ router.post("/logout", logout);
 router.put("/update-profile", protectRoute, updateProfile);
 
 router.get("/check", protectRoute, checkAuth);
+router.get("/last-seen/:id", protectRoute, getLastSeen);
+
+router.delete("/delete-profile", protectRoute, deleteProfile);
 
 export default router;

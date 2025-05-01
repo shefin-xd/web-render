5ea1f959d8d815c902f08a609f5db7be5ebc84c1
shefin. <shefinfranklin03@gmail.com>
2025-05-01 20:55:11 +0530

    Update Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -138,10 +138,11 @@ const Sidebar = () => {
             </div>
           </button>
         ))
+      )}
         {filteredUsers.length === 0 && (
           <div className="text-center text-zinc-500 py-4">No online users</div>
         )}
-      )}
+        
       </div>
     </aside>
   );

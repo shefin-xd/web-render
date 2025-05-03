71cb636b25bc1aeef0e10fda0496e80d0ac79f1d
shefin. <shefinfranklin03@gmail.com>
2025-05-03 14:30:54 +0530

    Update MainContainer.jsx

--- frontend/src/components/MainContainer.jsx
+++ frontend/src/components/MainContainer.jsx
@@ -1,4 +1,4 @@
-import { MessageSquare } from "lucide-react";
+import { MessageSquare, Zap } from "lucide-react";
 
 const MainContainer = () => {
   return (
@@ -11,13 +11,13 @@ const MainContainer = () => {
               className="w-16 h-16 rounded-2xl bg-primary/10 flex items-center
              justify-center animate-bounce"
             >
-              <MessageSquare className="w-8 h-8 text-primary " />
+              <Zap className="w-8 h-8 text-primary " />
             </div>
           </div>
         </div>
 
         {/* Welcome Text */}
-        <h2 className="text-2xl font-bold">Welcome to Messenger!</h2>
+        <h2 className="text-2xl font-bold">Welcome!</h2>
         <p className="text-base-content/60">
           Select a conversation from the sidebar to start chatting
         </p>

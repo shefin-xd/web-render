5e5f5829226d05267cbe5458936213215da712d7
shefin. <shefinfranklin03@gmail.com>
2025-05-03 12:57:20 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
                 <Zap className="w-5 h-5 text-primary" />
               </div>
-              <h1 className="text-lg font-bold">Messenger</h1>
+              <h1 className="text-lg font-bold">Shefin</h1>
             </Link>
           </div>
 

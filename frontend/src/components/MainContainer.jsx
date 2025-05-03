8533774cd1e82a76fd006321a831e86edc764cc2
shefin. <shefinfranklin03@gmail.com>
2025-05-03 17:53:18 +0530

    Update MainContainer.jsx

--- frontend/src/components/MainContainer.jsx
+++ frontend/src/components/MainContainer.jsx
@@ -1,8 +1,6 @@
 import { Zap } from "lucide-react";
-import { useAuthStore } from "../store/useAuthStore";
 
 const MainContainer = () => {
-  const { authUser } = useAuthStore();
   return (
     <div className="w-full flex flex-1 flex-col items-center justify-center p-16 bg-base-100/50">
       <div className="max-w-md text-center space-y-6">
@@ -19,7 +17,7 @@ const MainContainer = () => {
         </div>
 
         {/* Welcome Text */}
-        <h2 className="text-2xl font-bold">Welcome {authUser?.fullName}</h2>
+        <h2 className="text-2xl font-bold">Welcome!</h2>
       </div>
     </div>
   );

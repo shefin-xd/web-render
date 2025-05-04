dbc512cb2526e1582a5302691e486c358138280b
shefin. <shefinfranklin03@gmail.com>
2025-05-04 22:09:37 +0530

    Update SignUpPage.jsx

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -145,8 +145,8 @@ const SignUpPage = () => {
       {/* right side */}
 
       <AuthImagePattern
-        title="Join our community"
-        subtitle="Connect with friends, share moments, and stay in touch with your loved ones."
+        title=""
+        subtitle=""
       />
     </div>
   );

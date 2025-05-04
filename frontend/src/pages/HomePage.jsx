30191aa313d2b1a6350ddd02d67170bfa7200e75
shefin. <shefinfranklin03@gmail.com>
2025-05-04 21:59:03 +0530

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useChatStore } from "../store/useChatStore";
 
-import MainContainer from "../components/MainContainer";
+import DashboardPage from "../components/DashboardPage";
 
 const HomePage = () => {
 
@@ -9,7 +9,7 @@ const HomePage = () => {
       <div className="flex items-center justify-center pt-20 px-4">
         <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
           <div className="flex h-full rounded-lg overflow-hidden">
-           <MainContainer />
+           <DashboardPage />
           </div>
         </div>
       </div>

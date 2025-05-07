a60a299714f5f51ff32a5ec04813ecd6382aae9f
shefin. <shefinfranklin03@gmail.com>
2025-05-07 15:12:46 +0530

    Create functions.js

---
+++ backend/src/lib/functions.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+export const getJson = async (url, options) => {
+  try {
+    options ? options : {};
+    const res = await axios({
+      method: "GET",
+      url: url,
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36",
+      },
+      ...options,
+    });
+    return res.data;
+  } catch (error) {
+    console.log("error in update getJson:", error.message);
+  }
+};
+
+export const getBuffer = async (url, options = {}) => {
+  try {
+    const res = await axios({
+      method: "get",
+      url,
+      headers: {
+        DNT: 1,
+        "Upgrade-Insecure-Request": 1,
+      },
+      ...options,
+      responseType: "arraybuffer",
+    });
+    return res.data;
+  } catch (error) {
+    console.log("error in update getBuffer:", error.message);
+  }
+};

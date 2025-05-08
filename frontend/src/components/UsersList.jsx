d3f90d2174f8732fdb20ef068a936411ef3d7a77
shefin. <shefinfranklin03@gmail.com>
2025-05-08 22:42:52 +0530

    Update UsersList.jsx

--- frontend/src/components/UsersList.jsx
+++ frontend/src/components/UsersList.jsx
@@ -18,9 +18,9 @@ const UsersList = () => {
 
   return (
     <div className="h-screen w-full flex flex-col">
-      <div className="border-b border-base-300 w-full p-5 flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <Users className="size-6 items-center" />
+      <div className="border-b border-base-300 w-full p-5 flex items-center justify-center">
+        <div className="flex items-center gap-2 justify-center">
+          <Users className="size-6" />
           <span className="font-medium text-center">Users List</span>
         </div>
       </div>

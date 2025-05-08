c8577584a215538f3838719b46b1a87c2abdf219
shefin. <shefinfranklin03@gmail.com>
2025-05-09 00:35:56 +0530

    Update useUserStore.js

--- frontend/src/store/useUserStore.js
+++ frontend/src/store/useUserStore.js
@@ -4,10 +4,8 @@ import { axiosInstance } from "../lib/axios";
 
 export const useUserStore = create((set, get) => ({
   users: [],
-  admins: [],
   selectedUser: null,
   isUsersLoading: false,
-  isAdminsLoading: false,
 
   getUsers: async () => {
     set({ isUsersLoading: true });
@@ -18,18 +16,6 @@ export const useUserStore = create((set, get) => ({
       toast.error(error.response.data.message);
     } finally {
       set({ isUsersLoading: false });
-    }
-  },
-
-  getAdmins: async () => {
-    set({ isAdminsLoading: true });
-    try {
-      const res = await axiosInstance.get("/user/get-admins");
-      set({ admins: res.data });
-    } catch (error) {
-      toast.error(error.response.data.message);
-    } finally {
-      set({ isAdminsLoading: false });
     }
   },
   

eb746ca96174917750e85b3138837dca8f2d91b8
shefin. <shefinfranklin03@gmail.com>
2025-05-08 23:41:22 +0530

    Update user.controller.js

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -2,9 +2,8 @@ import User from "../models/user.model.js";
 
 export const getUsers = async (req, res) => {
   try {
-    const adminId = req.user._id;
-    const filteredUsers = await User.find({ _id: { $ne: adminId } }).select("-password");
-    res.status(200).json(filteredUsers);
+    const Users = await User.find({}).select("-password");
+    res.status(200).json(Users);
   } catch (error) {
     console.error("Error in getUsers: ", error.message);
     res.status(500).json({ error: "Internal server error" });

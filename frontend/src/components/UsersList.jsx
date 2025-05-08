fa405d1626cf6f431bbca2852e0a89817330b42c
shefin. <shefinfranklin03@gmail.com>
2025-05-08 17:43:26 +0530

    Update UsersList.jsx

--- frontend/src/components/UsersList.jsx
+++ frontend/src/components/UsersList.jsx
@@ -7,17 +7,7 @@ const UsersList = () => {
   return (
     <div className="w-full flex flex-1 flex-col items-center justify-center p-16 bg-base-100/50">
       <div className="max-w-md text-center space-y-6">
-        {/* Icon Display */}
-        <div className="flex justify-center gap-4 mb-4">
-          <div className="relative">
-            <div
-              className="w-16 h-16 rounded-2xl bg-primary/10 flex items-center
-             justify-center animate-bounce"
-            >
-              <Zap className="w-8 h-8 text-primary " />
-            </div>
-          </div>
-        </div>
+        
 
         {/* Welcome Text */}
         <h2 className="text-2xl font-bold">Hi {authUser?.name}!</h2>

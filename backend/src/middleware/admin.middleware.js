7595bb7dd1ec0e8e47ee84e709f1cab4a8f634f6
shefin. <shefinfranklin03@gmail.com>
2025-05-08 22:04:48 +0530

    Update admin.middleware.js

--- backend/src/middleware/admin.middleware.js
+++ backend/src/middleware/admin.middleware.js
@@ -2,18 +2,15 @@ import User from "../models/user.model.js";
 
 export const adminRoute = async (req, res, next) => {
   try {
-    const userId = req.user._id;
-    const role = await User.findById(userId).select("role");
-
-    const admin = (role == "admin") ? true : false;
-
-    if (!admin) {
+    
+    if (req.user && req.user.role === "admin") {
+      next();
+    } else {
       return res.status(401).json({ message: "Access denied - Admin only" });
     }
-
-    next();
+    
   } catch (error) {
-    console.log("Error in isAdmin middleware: ", error.message);
+    console.log("Error in adminRoute middleware: ", error.message);
     res.status(500).json({ message: "Internal server error" });
   }
 };

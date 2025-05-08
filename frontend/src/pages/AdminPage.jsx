92fefdc41b619baac34d3729ad815338442e8beb
shefin. <shefinfranklin03@gmail.com>
2025-05-08 16:25:09 +0530

    Create AdminPage.jsx

---
+++ frontend/src/pages/AdminPage.jsx
@@ -0,0 +1,20 @@
+import { useUserStore } from "../store/useUserStore";
+
+//import UsersList from "../components/UsersList";
+
+const AdminPage = () => {
+
+  return (
+    <div className="h-screen bg-base-200">
+      <div className="flex items-center justify-center pt-20 px-4">
+        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+          <div className="flex h-full rounded-lg overflow-hidden">
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminPage;

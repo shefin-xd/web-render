61548ec9d3aa10b62041d76bb38f63fa8502b690
shefin. <shefinfranklin03@gmail.com>
2025-05-08 17:18:10 +0530

    Update UsersList.jsx

--- frontend/src/components/UsersList.jsx
+++ frontend/src/components/UsersList.jsx
@@ -18,36 +18,8 @@ const UsersList = () => {
   return (
     <div className="w-full flex flex-1 flex-col items-center justify-center p-16 bg-base-100/50">
       <div className="max-w-md text-center space-y-6">
+        <h1>Shefin</h1>
 
-        <div className="overflow-y-auto w-full py-3">
-        {filteredUsers.map((user) => (
-          <button
-            key={user._id}
-            onClick={() => setSelectedUser(user)}
-            className={`
-              w-full p-3 flex items-center gap-3
-              hover:bg-base-300 transition-colors
-              ${selectedUser?._id === user._id ? "bg-base-300 ring-1 ring-base-300" : ""}
-            `}
-          >
-            <div className="relative mx-auto lg:mx-0">
-              <img
-                src={user.profilePic || "/avatar.png"}
-                alt={user.name}
-                className="size-12 object-cover rounded-full"
-              />
-              {onlineUsers.includes(user._id) && (
-                <span
-                  className="absolute bottom-0 right-0 size-3 bg-green-500 
-                  rounded-full ring-2 ring-zinc-900"
-                />
-              )}
-            </div>
-
-            
-          </button>
-        ))}
-      </div>
         
       </div>
     </div>

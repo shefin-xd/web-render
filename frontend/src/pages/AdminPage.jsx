6d4eaeeb117367c4f7e84d03194bdf474bca1699
shefin. <shefinfranklin03@gmail.com>
2025-05-08 17:08:58 +0530

    Update AdminPage.jsx

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,6 +1,6 @@
 import { useUserStore } from "../store/useUserStore";
 
-//import UsersList from "../components/UsersList";
+import UsersList from "../components/UsersList";
 
 const AdminPage = () => {
 
@@ -9,7 +9,7 @@ const AdminPage = () => {
       <div className="flex items-center justify-center pt-20 px-4">
         <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
           <div className="flex h-full rounded-lg overflow-hidden">
-           
+           <UsersList />
           </div>
         </div>
       </div>

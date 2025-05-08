c3db58fca2aec736539ca499edf27775d8b0e37a
shefin. <shefinfranklin03@gmail.com>
2025-05-09 00:31:32 +0530

    Update UsersList.jsx

--- frontend/src/components/UsersList.jsx
+++ frontend/src/components/UsersList.jsx
@@ -13,7 +13,7 @@ const UsersList = () => {
   }, [getUsers]);
 
   const filteredUsers = showOnlineOnly
-    ? users.filter((user) => onlineUsers.includes(user._id))
+    ? users.filter((user) => user.role === "admin")
     : users;
 
   return (

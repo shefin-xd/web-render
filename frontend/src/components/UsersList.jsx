ef62feac010d2808dfcc9ff799e389419c21d152
shefin. <shefinfranklin03@gmail.com>
2025-05-08 17:24:04 +0530

    Update UsersList.jsx

--- frontend/src/components/UsersList.jsx
+++ frontend/src/components/UsersList.jsx
@@ -1,25 +1,12 @@
-import { useEffect, useState } from "react";
-import { useUserStore } from "../store/useUserStore";
 import { useAuthStore } from "../store/useAuthStore";
-import { Users } from "lucide-react";
 
 const UsersList = () => {
-  const { getUsers, users, selectedUser, setSelectedUser, isUsersLoading } = useUserStore();
-  const { onlineUsers } = useAuthStore();
-  
-  useEffect(() => {
-    getUsers();
-  }, [getUsers]);
-
-  const filteredUsers = showOnlineOnly
-    ? users.filter((user) => onlineUsers.includes(user._id))
-    : users;
 
   return (
     <div className="w-full flex flex-1 flex-col items-center justify-center p-16 bg-base-100/50">
       <div className="max-w-md text-center space-y-6">
-        <h1>Shefin</h1>
 
+        <h1> Test </h1>
         
       </div>
     </div>
@@ -27,4 +14,3 @@ const UsersList = () => {
 };
 
 export default UsersList;
-

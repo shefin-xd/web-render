3b1f72c9d3d326d05e735edf1f10a28e43823c84
shefin. <shefinfranklin03@gmail.com>
2025-05-08 18:46:59 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,12 +32,13 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Hey Guys",
-    runtime: getRunTime(),
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
-});
+}
 
 connectDB().then(()=>{
     server.listen(PORT, () => {

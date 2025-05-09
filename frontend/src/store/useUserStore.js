4a7908cdcf39ee62bc88e21f7222a3dd5bdfde78
shefin. <shefinfranklin03@gmail.com>
2025-05-09 18:10:02 +0530

    Update useUserStore.js

--- frontend/src/store/useUserStore.js
+++ frontend/src/store/useUserStore.js
@@ -5,17 +5,42 @@ import { axiosInstance } from "../lib/axios";
 export const useUserStore = create((set, get) => ({
   users: [],
   selectedUser: null,
+  isDeletingUser: false,
+  isTogglingAdmin: false,
   isUsersLoading: false,
 
   getUsers: async () => {
     set({ isUsersLoading: true });
     try {
-      const res = await axiosInstance.get("/user/get-users");
+      const res = await axiosInstance.get("/users/get-users");
       set({ users: res.data });
     } catch (error) {
       toast.error(error.response.data.message);
     } finally {
       set({ isUsersLoading: false });
+    }
+  },
+
+  deleteUser: async (userId) => {
+    set({ isDeletingUser: true });
+    try {
+      const res = await axiosInstance.delete(`/users/${userId}`);
+      set({ users: users.filter((user) => user._id !== userId) });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isDeletingUser: false });
+    }
+  },
+  
+  toggleAdmin: async (userId) => {
+    set({ isTogglingAdmin: true });
+    try {
+      const response = await axios.patch(`/users/${userId}`);
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isTogglingAdmin: false });
     }
   },
   

9502a99cd0390f61eab579cc0db4a5cf5d5f9a32
shefin. <shefinfranklin03@gmail.com>
2025-05-09 08:37:34 +0530

    Update UsersListSkeleton.jsx

--- frontend/src/components/skeletons/UsersListSkeleton.jsx
+++ frontend/src/components/skeletons/UsersListSkeleton.jsx
@@ -1,47 +1,38 @@
 import { Users } from "lucide-react";
 
 const UsersListSkeleton = () => {
-  const skeletonUsers = Array(8).fill(null);
+  // Create 8 skeleton items
+  const skeletonContacts = Array(8).fill(null);
 
   return (
-    <div className="h-screen w-full flex flex-col transition-all duration-200">
-      <div className="border-b border-base-300 w-full p-5 flex items-center justify-between">
+    <div
+      className="h-screen w-full border-r border-base-300 
+    flex flex-col transition-all duration-200"
+    >
+      {/* Header */}
+      <div className="border-b border-base-300 w-full p-5">
         <div className="flex items-center gap-2">
-          <Users className="size-6" />
-          <span className="font-medium">Users List</span>
+          <Users className="w-6 h-6" />
+          <span className="font-medium hidden lg:block">Contacts</span>
         </div>
       </div>
 
+      {/* Skeleton Contacts */}
       <div className="overflow-y-auto w-full py-3 flex-grow">
-        <div className="container mx-auto p-4">
-           {skeletonUsers.map((_, idx) => (
-            <div key={idx}>
-              
-                <span className="font-medium">{idx + 1}.</span> {/* Numbering */}
-                <div className="relative">
-                  <img
-                    src={"/avatar.png"}
-                    alt={"shefin"}
-                    className="size-12 object-cover rounded-full"
-                  />
-                  {onlineUsers.includes(user._id) && (
-                    <span
-                      className="absolute bottom-0 right-0 size-3 bg-green-500 
-                      rounded-full ring-2 ring-zinc-900"
-                    />
-                  )}
-                </div>
-
-                {/* User info - name and email */}
-                <div className="flex flex-col text-left min-w-0 ml-2">
-                  <div className="font-medium truncate">Name</div>
-                  <div className="text-sm text-zinc-400 truncate">email</div>
-                </div>
-              <hr className="border-t border-base-300" /> {/* Divider line */}
+        {skeletonContacts.map((_, idx) => (
+          <div key={idx} className="w-full p-3 flex items-center gap-3">
+            {/* Avatar skeleton now alongside username */}
+            <div className="relative">
+              <div className="skeleton size-12 rounded-full" />
             </div>
-          ))}
 
-        </div>
+            {/* User info skeleton, displayed alongside avatar */}
+            <div className="flex flex-col justify-center min-w-0 flex-1">
+              <div className="skeleton h-4 w-32 mb-2" />
+              <div className="skeleton h-3 w-16" />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );

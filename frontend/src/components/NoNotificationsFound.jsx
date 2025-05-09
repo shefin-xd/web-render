1283db89cee7a9af03073c877d15923a8ce4ea97
shefin. <shefinfranklin03@gmail.com>
2025-05-09 23:06:01 +0530

    Update NoNotificationsFound.jsx

--- frontend/src/components/NoNotificationsFound.jsx
+++ frontend/src/components/NoNotificationsFound.jsx
@@ -2,7 +2,7 @@ import { BellIcon } from "lucide-react";
 
 const NoNotificationsFound = () => {
   return (
-    <div className="flex flex-col items-center justify-center py-16 text-center">
+    <div className="h-screen w-full flex flex-col items-center justify-center text-center">
       <div className="size-16 rounded-full bg-base-300 flex items-center justify-center mb-4">
         <BellIcon className="size-8 text-base-content opacity-40" />
       </div>

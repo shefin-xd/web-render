d2444b556708382bc1c933c6b6d1688b4e9806fc
shefin. <shefinfranklin03@gmail.com>
2025-05-09 08:41:02 +0530

    Update UsersListSkeleton.jsx

--- frontend/src/components/skeletons/UsersListSkeleton.jsx
+++ frontend/src/components/skeletons/UsersListSkeleton.jsx
@@ -31,6 +31,9 @@ const UsersListSkeleton = () => {
               <div className="skeleton h-4 w-32 mb-2" />
               <div className="skeleton h-3 w-16" />
             </div>
+
+            {/* Numbering the skeletons */}
+            <div className="text-gray-500 font-medium">{idx + 1}</div>
           </div>
         ))}
       </div>

c3903b87c8e80d5a00e975e4bba9fc40dfc46462
shefin. <shefinfranklin03@gmail.com>
2025-05-09 17:54:15 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,7 +30,7 @@ app.use(
 );
 
 app.use("/api/auth", authRoutes);
-app.use("/api/user", userRoutes);
+app.use("/api/users", userRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));

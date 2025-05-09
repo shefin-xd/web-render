862256929bca67bf8ffe5c797ae9dadf03816e91
shefin. <shefinfranklin03@gmail.com>
2025-05-09 17:43:53 +0530

    Update user.controller.js

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -10,3 +10,38 @@ export const getUsers = async (req, res) => {
   }
 };
 
+export const deleteUser = async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id);
+		if (!user) {
+			return res.status(400).json({ message: "User not found" });
+		}
+    
+		await Product.findByIdAndDelete(req.params.id);
+    
+		res.status(200).json({ message: "Account deleted successfully" });
+  } catch (error) {
+    console.log("error in delete profile:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const toggleAdmin = async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id);
+		if (user) {
+      if (user.role === "admin") {
+        user.role = "user"
+      } else if (user.role === "user") {
+        user.role = "admin"
+      }
+			const updatedUser = await user.save();
+			res.json(updatedUser);
+		} else {
+			res.status(400).json({ message: "Product not found" });
+		}
+	} catch (error) {
+		console.log("error in toggleAdmin controller", error);
+		res.status(500).json({ message: "Internal server error" });
+  }
+};

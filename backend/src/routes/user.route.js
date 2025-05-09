9a055ca9705103846e6f05e126e2bb190119fe67
shefin. <shefinfranklin03@gmail.com>
2025-05-09 17:49:13 +0530

    Update user.route.js

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import { adminRoute } from "../middleware/admin.middleware.js";
-import { getUsers } from "../controllers/user.controller.js";
+import { getUsers, toggleAdmin, deleteUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/get-users", protectRoute, adminRoute, getUsers);
+
+router.patch("/:id", protectRoute, adminRoute, toggleAdmin);
+
+router.delete("/:id", protectRoute, adminRoute, deleteUser);
 
 export default router;
